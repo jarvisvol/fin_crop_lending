0f608ac3d658f91c188ce6c0937dece172527fa4
Shubham <shubham1410.ss@gmail.com>
2025-12-19 16:07:50 +0530

    ch

--- config/jwt.php
+++ config/jwt.php
@@ -1,24 +1,15 @@
 <?php
 
 return [
-
     'secret' => env('JWT_SECRET'),
-
-
-    'keys' => [ 
-
+    'keys' => [
         'public' => env('JWT_PUBLIC_KEY'),
-
         'private' => env('JWT_PRIVATE_KEY'),
-
         'passphrase' => env('JWT_PASSPHRASE'),
-
     ],
-
-    'ttl' => env('JWT_TTL', 60),
-
-    'algo' => env('JWT_ALGO', 'HS256'),
-
+    'ttl' => (int) env('JWT_TTL', 60), // 24 hours
+    'refresh_ttl' => env('JWT_REFRESH_TTL', 20160), // 2 weeks
+    'algo' => env('JWT_ALGO', 'RS256'), // Change to RS256
     'required_claims' => [
         'iss',
         'iat',
@@ -27,31 +18,16 @@ return [
         'sub',
         'jti',
     ],
-
-    'persistent_claims' => [
-        // 'foo',
-        // 'bar',
-    ],
-
+    'persistent_claims' => [],
+    'lock_subject' => true,
     'leeway' => env('JWT_LEEWAY', 0),
-
-
     'blacklist_enabled' => env('JWT_BLACKLIST_ENABLED', true),
-
     'blacklist_grace_period' => env('JWT_BLACKLIST_GRACE_PERIOD', 0),
-
-
-
     'decrypt_cookies' => false,
-
     'providers' => [
-
         'jwt' => Tymon\JWTAuth\Providers\JWT\Lcobucci::class,
-
         'auth' => Tymon\JWTAuth\Providers\Auth\Illuminate::class,
-
         'storage' => Tymon\JWTAuth\Providers\Storage\Illuminate::class,
-
+        'request' => Tymon\JWTAuth\Providers\Request\Illuminate::class,
     ],
-
 ];
